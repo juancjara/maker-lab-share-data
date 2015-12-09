9cd02ffc7f56fb9713bfa87e6be05d9a78d2cf77
juancarlos <juanc.jara@pucp.pe>
2015-12-09 00:40:10 -0500

    feature: adding msg, xxs, autolinker

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,28 @@
-let obj = {cad: 'asdf'};
+import Vue from 'vue';
+import Autolinker from 'autolinker';
+import xss from 'xss';
 
-let test = ({cad}) => console.log(cad);
+let cleanAndAutolink = (msg) => Autolinker.link(xss(msg).trim());
 
-test(obj);
+let app = new Vue({
+
+  el: "#app",
+
+  data: {
+    newMsg: '',
+    messages: []
+  },
+
+  methods: {
+
+    addMsg() {
+      let msg = cleanAndAutolink(this.newMsg);
+      if (msg) {
+        this.messages.unshift(msg);
+        this.newMsg = '';
+      }
+    }
+
+  }
+
+});
